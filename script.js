c9cae08dfcc8670ef5297278eaff1f524e04973e
Дмитрий <Skrevo@outlook.com>
2022-11-11 10:06:11 +0200

    optimize for any number of currency

--- script.js
+++ script.js
@@ -5,97 +5,87 @@ currencyApp.controller('CurrencyCtrl', function ($scope, $http){
         $scope.currency = data;
     });
 });
-// получение курса валют для конвертации
-let curUSDtoUAH
-let curEURtoUAH
-let requestURL = 'https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11'
-let request = new XMLHttpRequest()
-request.open('GET',requestURL)
-request.responseType = 'json';
-request.send();
-request.onload = function() {
-   let curInfo = request.response;
-    console.log(curInfo)
-   curUSDtoUAH = curInfo[0].sale;
-   curEURtoUAH =curInfo[1].sale;
-}
+
+// Получение значений полей
+let setCur = document.getElementById("select1")
+let setCur2 = document.getElementById("select2")
+let input1 = document.getElementById("in")
+let input2 = document.getElementById("out")
 
 // ожидание изменений выбора валюты или изменения суммы
-let changeCur = document.getElementById("select1")
-changeCur.addEventListener('click', function(){
+setCur.addEventListener('click', function(){
     calc1()
 })
-let changeCur2 = document.getElementById("select2")
-changeCur2.addEventListener('click', function(){
+setCur2.addEventListener('click', function(){
     calc1()
 })
-let changeSum = document.getElementById("in")
-changeSum.addEventListener('input', function(){
+input1.addEventListener('input', function(){
     calc1()
 })
-let changeSum2 = document.getElementById("out")
-changeSum2.addEventListener('input', function(){
+input2.addEventListener('input', function(){
     calc2()
 })
 
+// подключение к API
+let requestURL = 'https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11'
+let request = new XMLHttpRequest()
+
 // функции конвертации
 let res
-function calc1(sum ,cur) {
-    sum = document.getElementById("in").value
-    if (document.getElementById("select1").value === "USD") {
-        if (document.getElementById("select2").value === "UAH")
-            cur = curUSDtoUAH
-        if (document.getElementById("select2").value === "EUR")
-            cur = curUSDtoUAH/curEURtoUAH
-        if (document.getElementById("select2").value === "USD")
-            cur = 1
+function calc1(sum,cur) {
+    sum = input1.value
+    if (setCur2.value === setCur.value)
+        cur = 1
+    request.open('GET',requestURL)
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+        let curInfo = request.response;
+        for (let c in curInfo) {
+            if (setCur.value === curInfo[c].ccy && setCur2.value === curInfo[c].base_ccy)
+                cur = curInfo[c].sale
+            if (setCur2.value === curInfo[c].ccy && setCur.value === curInfo[c].base_ccy)
+                cur = 1/curInfo[c].sale
+            if (setCur.value === curInfo[c].ccy && setCur2.value !== curInfo[c].base_ccy) {
+                if (setCur2.value !== setCur.value) {
+                    let findCur2 = function () {
+                        for (let f in curInfo)
+                            if (setCur2.value === curInfo[f].ccy) return curInfo[f].sale
+                    }
+                    cur = curInfo[c].sale / findCur2()
+                }
+            }
+        }
+        res = cur * sum
+        input2.value=res
     }
-    if (document.getElementById("select1").value === "EUR") {
-        if (document.getElementById("select2").value === "UAH")
-            cur = curEURtoUAH
-        if (document.getElementById("select2").value === "EUR")
-            cur = 1
-        if (document.getElementById("select2").value === "USD")
-            cur = curEURtoUAH/curUSDtoUAH
-    }
-    if (document.getElementById("select1").value === "UAH") {
-        if (document.getElementById("select2").value === "UAH")
-            cur = 1
-        if (document.getElementById("select2").value === "EUR")
-            cur = 1/curEURtoUAH
-        if (document.getElementById("select2").value === "USD")
-            cur = 1/curUSDtoUAH
-    }
-    res = cur * sum
-    document.getElementById("out").value=res
 }
 
 function calc2(sum ,cur) {
-    sum = document.getElementById("out").value
-    if (document.getElementById("select2").value === "USD") {
-        if (document.getElementById("select1").value === "UAH")
-            cur = curUSDtoUAH
-        if (document.getElementById("select1").value === "EUR")
-            cur = curUSDtoUAH/curEURtoUAH
-        if (document.getElementById("select1").value === "USD")
-            cur = 1
-    }
-    if (document.getElementById("select2").value === "EUR") {
-        if (document.getElementById("select1").value === "UAH")
-            cur = curEURtoUAH
-        if (document.getElementById("select1").value === "EUR")
-            cur = 1
-        if (document.getElementById("select1").value === "USD")
-            cur = curEURtoUAH/curUSDtoUAH
-    }
-    if (document.getElementById("select2").value === "UAH") {
-        if (document.getElementById("select1").value === "UAH")
-            cur = 1
-        if (document.getElementById("select1").value === "EUR")
-            cur = 1/curEURtoUAH
-        if (document.getElementById("select1").value === "USD")
-            cur = 1/curUSDtoUAH
-    }
+    sum = input2.value
+    if (setCur2.value === setCur.value)
+        cur = 1
+    request.open('GET',requestURL)
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+        let curInfo = request.response;
+        for (let c in curInfo) {
+            if (setCur2.value === curInfo[c].ccy && setCur.value === curInfo[c].base_ccy)
+                cur = curInfo[c].sale
+            if (setCur.value === curInfo[c].ccy && setCur2.value === curInfo[c].base_ccy)
+                cur = 1/curInfo[c].sale
+            if (setCur2.value === curInfo[c].ccy && setCur.value !== curInfo[c].base_ccy) {
+                if (setCur2.value !== setCur.value) {
+                    let findCur2 = function () {
+                        for (let f in curInfo)
+                            if (setCur.value === curInfo[f].ccy) return curInfo[f].sale
+                    }
+                    cur = findCur2() / curInfo[c].sale
+                }
+            }
+        }
     res = cur * sum
-    document.getElementById("in").value=res
+    input1.value=res
+    }
 }
